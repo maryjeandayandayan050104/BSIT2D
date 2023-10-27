4e8a91c16d29327bab3eaa1216200fccec5e3090
maryjeandayandayan050104 <105650244+maryjeandayandayan050104@users.noreply.github.com>
2023-10-27 15:46:15 +0800

    create

---
+++ JAVA/example.java
@@ -0,0 +1,15 @@
+/*
+ * NAME         : DAYANDAYAN, MARY JEAN C.
+ * YEAR/SECTION : BSIT 2D
+ * Lab Example 1
+ */
+
+public class example{
+public static void main(String []args){                                           
+    System.out.println("MARY JEAN CAYLAN DAYANDAYAN");       
+    System.out.println("SAN ISIDRO");
+    System.out.println("19");               
+    System.out.println("MAY 01 2004");
+    System.out.println("0985 889 6551");
+    }
+}
